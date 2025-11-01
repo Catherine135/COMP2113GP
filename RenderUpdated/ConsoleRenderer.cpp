a68739bcce27fa13dbc032d02432ca812844747e
Chen Ziqi <u3629231@connect.hku.hk>
2025-11-01 19:01:31 +0800

    Updated Render
    A new structure NameInterface and a new ConsoleRenderer function renderNameInterface for the user to input username are added. However, this contains only a basic UI of displaying user input. Rules of setting username and methods for saving user information and writing user information into files are currently not specified.
    In main.cpp, after running all the render tasks in the vector, the a NameInterface object is created and renderNameInterface is called to show the rendering of the NameInterface.
    Other minor changes (such as defining more macro that represent setting text colors and clearing the screen) are made to make the code neater.

---
+++ RenderUpdated/ConsoleRenderer.cpp
@@ -0,0 +1,478 @@
+#include "ConsoleRenderer.h"
+#include <iostream>
+#include <iomanip>
+#include <typeinfo>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+#define DFT "\033[0m"
+#define RED_BG "\033[41m"
+#define GREEN_BG "\033[42m"
+#define YELLOW_BG "\033[43m"
+#define CYAN_BG "\033[46m"
+#define WHITE_BG "\033[47m"
+#define RED "\033[31m"
+#define BLUE "\033[34m"
+#define CLEAR "\033c"
+
+/// @brief moves the cursor to the assigned position
+/// @param row row position for the cursor to move to
+/// @param col column position for the cursor to move to
+void moveCursor(int row, int col){
+    std::cout << "\033[" << row << ";" << col << "H";
+}
+
+/// @brief return the type of the target tile (capital, city, mountain, quagmire,none)
+/// @param tile the target tile to get the symbol
+/// @return a character representing the type of the tile (capital, city, mountain, quagmire,none)
+char getTileSymbol(const Tile& tile) {
+    if (tile.isMountain) return '^';
+    if (tile.isCity) return 'C';
+    if (tile.isCapital) return '*';
+    if (tile.isQuagmire) return '~';
+    return '.';
+}
+
+/// @brief assign the rendering task to specific ConsoleRenderer functions
+/// @param task the rendering task
+void ConsoleRenderer::render(const RenderTask& task) {
+    if (const auto* t = dynamic_cast<const SelectTile*>(&task)) {
+        renderSelectTile(*t);
+    } else if (const auto* t = dynamic_cast<const Arrow*>(&task)) {
+        renderArrow(*t);
+    } else if (const auto* t = dynamic_cast<const RefreshMap*>(&task)) {
+        renderRefreshMap(*t);
+    } else if (const auto* t = dynamic_cast<const UpdateTile*>(&task)) {
+        renderUpdateTile(*t);
+    } else if (const auto* t = dynamic_cast<const ShowMsg*>(&task)) {
+        renderShowMsg(*t);
+    } else if (const auto* t = dynamic_cast<const GameInterface*>(&task)) {
+        renderGameInterface(*t);
+    } else if (const auto* t = dynamic_cast<const StartInterface*>(&task)) {
+        renderStartInterface(*t);
+    } else if (const auto* t = dynamic_cast<const NameInterface*>(&task)){
+        renderNameInterface(*t);
+    } else if (const auto* t = dynamic_cast<const DifficultyInterface*>(&task)) {
+        renderDifficultyInterface(*t);
+    } else if (const auto* t = dynamic_cast<const PauseInterface*>(&task)){
+        renderPauseInterface(*t);
+    } else if (const auto* t = dynamic_cast<const EndGameInterface*>(&task)){
+        renderEndGameInterface(*t);
+    } else if (const auto* t = dynamic_cast<const LeaderboardInterface*>(&task)){
+        renderLeaderboardInterface(*t);
+    }
+}
+
+/// @brief highlight the selected tile into yellow, or turn the tile back to red if it is not selected
+///        Given the rules of generals.io, only the player's tiles can be highlighted.
+/// @param task the SelectTile rendering task
+void ConsoleRenderer::renderSelectTile(const SelectTile& task) {
+    int x = task.coords.first;
+    int y = task.coords.second;
+    Tile tile = task.snapshot[y][x];
+    int row = 2 * y + 13;
+    int col = x * 4 + 24 + 1;
+    if (task.select){
+        if (!tile.army){
+            moveCursor(row,col);
+            std::cout << YELLOW_BG << "  " << DFT;
+        } else{
+            moveCursor(row,col);
+            std::cout << YELLOW_BG << std::setw(2) << tile.army << DFT << std::flush;
+        }
+    } else {
+        if (!tile.army){
+            moveCursor(row,col);
+            std::cout << RED_BG << "  " << DFT;
+        } else{
+            moveCursor(row,col);
+            std::cout << RED_BG << std::setw(2) << tile.army << DFT << std::flush;
+        }
+    }
+    
+
+    moveCursor(40,0);
+    std::cout.flush();
+}
+
+/// @brief draw an arrow between adjacent tiles, or erase the arrow between adjacent tiles
+///        Given the rules of generals.io, only arrows on the player's side is generated.
+/// @param task the Arrow rendering task
+void ConsoleRenderer::renderArrow(const Arrow& task) {
+    int x0 = task.from.first;
+    int y0 = task.from.second;
+    int x1 = task.to.first;
+    int y1 = task.to.second;
+
+    Tile tile = task.snapshot[y0][x0];
+
+    if ((x0 == x1) && (y0 - y1 == 1)){
+        int row = 2 * y0 + 13 -1;
+        int col = x0 * 4 + 24 + 1;
+        moveCursor(row,col);
+        if (task.draw){std::cout << "^";} 
+        else {std::cout << " ";}
+    }
+    else if ((x0 == x1) && (y0 - y1 == -1)){
+        int row = 2 * y0 + 13 + 1;
+        int col = x0 * 4 + 24 + 1;
+        moveCursor(row,col);
+        if (task.draw){std::cout << "v";} 
+        else {std::cout << " ";}
+    }
+    else if ((x0 - x1 == 1) && (y0 == y1)){
+        int row = 2 * y0 + 13;
+        int col = x0 * 4 + 24 - 1;
+        moveCursor(row,col);
+        if (task.draw){std::cout  << "<";} 
+        else {std::cout << " ";}
+    }
+    else if ((x0 - x1 == -1) && (y0 == y1)){
+        int row = 2 * y0 + 13;
+        int col = x0 * 4 + 24 +3;
+        moveCursor(row,col);
+        if (task.draw){std::cout << ">";} 
+        else {std::cout << " ";}
+    }
+
+    moveCursor(40,0);
+    std::cout.flush();
+}
+
+/// @brief update the whole game board (map)
+/// @param task the RefreshMap rendering task
+void ConsoleRenderer::renderRefreshMap(const RefreshMap& task) {    
+    for (int y = 0; y < task.snapshot.size(); y++) {
+        for (int x = 0; x < task.snapshot[y].size(); x++) {
+            int row = 2 * y + 13;
+            int col = x * 4 + 24;
+            Tile tile = task.snapshot[y][x];
+            
+            moveCursor(row, col);
+            if (tile.isMountain){std::cout << WHITE_BG << getTileSymbol(tile) << "  ";}
+            else if (tile.army){
+                if (tile.owner == 1) {std::cout << RED_BG << getTileSymbol(tile) << std::setw(2) << tile.army;}
+                else if (tile.owner == 0){std::cout << GREEN_BG << getTileSymbol(tile) << std::setw(2) << tile.army; }
+                else {std::cout << WHITE_BG << getTileSymbol(tile) << std::setw(2) << tile.army; }
+            } else {
+                std::cout << WHITE_BG << getTileSymbol(tile) << "  ";
+            }
+
+            std::cout << DFT;
+        }
+    }
+
+    moveCursor(40,0);
+    std::cout.flush();
+}
+
+/// @brief update a certain file
+/// @param task the UpdateTile rendering task
+void ConsoleRenderer::renderUpdateTile(const UpdateTile& task) {
+    int x = task.coords.first;
+    int y = task.coords.second;
+    int row = 2 * y + 13;
+    int col = x * 4 + 24;
+
+    moveCursor(row, col);
+    if (task.tile.isMountain){std::cout << WHITE_BG << getTileSymbol(task.tile) << "  ";}
+    else if (task.tile.army){
+        if (task.tile.owner == 1) {std::cout << RED_BG << getTileSymbol(task.tile) << std::setw(2) << task.tile.army;}
+        else if (task.tile.owner == 0){std::cout << GREEN_BG << getTileSymbol(task.tile) << std::setw(2) << task.tile.army; }
+        else {std::cout << WHITE_BG << getTileSymbol(task.tile) << std::setw(2) << task.tile.army; }
+    } else {
+        std::cout << WHITE_BG << getTileSymbol(task.tile) << "  ";
+    }
+    std::cout <<DFT;
+    
+    moveCursor(40,0);
+    std::cout.flush();
+}
+
+/// @brief shows the message of the game (turns, players, land, army, or some other messages)
+/// @param task the ShowMsg rendering task
+void ConsoleRenderer::renderShowMsg(const ShowMsg& task) {
+    moveCursor(12,72);
+    std::cout << WHITE_BG << "\033[34m" << "TURNS: ";
+    moveCursor(14,72);
+    std::cout <<"PLAYER" << std::setw(12) << "LAND" << std::setw(12) << "ARMY";
+    std::cout << DFT;
+    moveCursor(15,72);
+    std::cout << "YOU";
+    moveCursor(16,72);
+    std::cout << "AI";
+    moveCursor(17,72);
+    std::cout << WHITE_BG << "\033[34m" << "Messages" << DFT;
+    moveCursor(18,72);
+    std::cout << task.msg << DFT;
+
+    moveCursor(40,0);
+    std::cout.flush();
+}
+
+/// @brief shows the gaming page
+/// @param task the GameInterface rendering task
+void ConsoleRenderer::renderGameInterface (const GameInterface& task){
+    std::cout << CLEAR;
+
+    moveCursor(3,10);
+    std::cout << " ██████╗ ███████╗███╗   ██╗███████╗██████╗  █████╗ ██╗███████╗   ██╗ ██████╗ ";
+    moveCursor(4,10);
+    std::cout << "██╔════╝ ██╔════╝████╗  ██║██╔════╝██╔══██╗██╔══██╗██║██╔════╝   ██║██╔═══██╗";
+    moveCursor(5,10);
+    std::cout << "██║  ███╗█████╗  ██╔██╗ ██║█████╗  ██████╔╝███████║██║███████╗   ██║██║   ██║";
+    moveCursor(6,10);
+    std::cout << "██║   ██║██╔══╝  ██║╚██╗██║██╔══╝  ██╔══██╗██╔══██║██║╚════██║   ██║██║   ██║";
+    moveCursor(7,10);
+    std::cout << "╚██████╔╝███████╗██║ ╚████║███████╗██║  ██║██║  ██║██║███████║██╗██║╚██████╔╝";
+    moveCursor(8,10);
+    std::cout << " ╚═════╝ ╚══════╝╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚══════╝╚═╝╚═╝ ╚═════╝ ";
+
+    moveCursor(11,7);
+    std::cout << "=========================== MAP ===========================";
+
+    moveCursor(11,72);
+    std::cout << "======== NOTICE BOARD ========";
+    
+   
+    moveCursor(21,72);
+    std::cout << WHITE_BG << "Press \"P\" to pause game.  ";
+    moveCursor(22,72);
+    std::cout << WHITE_BG << "Press \"R\" to resume game. ";
+    std::cout << DFT;
+    moveCursor(23,72);
+    std::cout << "==============================" << DFT;
+
+    moveCursor(40,0);
+    std::cout.flush();
+}
+
+/// @brief shows the starting page
+/// @param task the StartInterface rendering task
+void ConsoleRenderer::renderStartInterface (const StartInterface& task){
+    std::cout << CLEAR;
+    moveCursor(3,10);
+    std::cout << " ██████╗ ███████╗███╗   ██╗███████╗██████╗  █████╗ ██╗███████╗   ██╗ ██████╗ ";
+    moveCursor(4,10);
+    std::cout << "██╔════╝ ██╔════╝████╗  ██║██╔════╝██╔══██╗██╔══██╗██║██╔════╝   ██║██╔═══██╗";
+    moveCursor(5,10);
+    std::cout << "██║  ███╗█████╗  ██╔██╗ ██║█████╗  ██████╔╝███████║██║███████╗   ██║██║   ██║";
+    moveCursor(6,10);
+    std::cout << "██║   ██║██╔══╝  ██║╚██╗██║██╔══╝  ██╔══██╗██╔══██║██║╚════██║   ██║██║   ██║";
+    moveCursor(7,10);
+    std::cout << "╚██████╔╝███████╗██║ ╚████║███████╗██║  ██║██║  ██║██║███████║██╗██║╚██████╔╝";
+    moveCursor(8,10);
+    std::cout << " ╚═════╝ ╚══════╝╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚══════╝╚═╝╚═╝ ╚═════╝ ";
+
+    moveCursor(13,23);
+    std::cout << WHITE_BG << RED << " Welcome to generAIs.io, a human vs AI board game!! ";
+    moveCursor(17,37);
+    std::cout << WHITE_BG << BLUE << " START        (PRESS 1) ";
+    moveCursor(19,37);
+    std::cout << WHITE_BG << BLUE << " LEADERBOARD  (PRESS 2) ";
+    moveCursor(21,37);
+    std::cout << WHITE_BG << BLUE << " EXIT         (PRESS 3) " << DFT;
+
+    moveCursor(40,0);
+    std::cout.flush();
+}
+
+/// @brief allows the user to set their username for the game
+///             further UI integration!!!!!!
+/// @param task the NameInterface task
+void ConsoleRenderer::renderNameInterface(const NameInterface& task){
+    std::string name;
+    std::cout << CLEAR;
+    moveCursor(3,10);
+    std::cout << " ██████╗ ███████╗███╗   ██╗███████╗██████╗  █████╗ ██╗███████╗   ██╗ ██████╗ ";
+    moveCursor(4,10);
+    std::cout << "██╔════╝ ██╔════╝████╗  ██║██╔════╝██╔══██╗██╔══██╗██║██╔════╝   ██║██╔═══██╗";
+    moveCursor(5,10);
+    std::cout << "██║  ███╗█████╗  ██╔██╗ ██║█████╗  ██████╔╝███████║██║███████╗   ██║██║   ██║";
+    moveCursor(6,10);
+    std::cout << "██║   ██║██╔══╝  ██║╚██╗██║██╔══╝  ██╔══██╗██╔══██║██║╚════██║   ██║██║   ██║";
+    moveCursor(7,10);
+    std::cout << "╚██████╔╝███████╗██║ ╚████║███████╗██║  ██║██║  ██║██║███████║██╗██║╚██████╔╝";
+    moveCursor(8,10);
+    std::cout << " ╚═════╝ ╚══════╝╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚══════╝╚═╝╚═╝ ╚═════╝ ";
+
+    moveCursor(13,28);
+    std::cout << WHITE_BG << RED << " Please enter your name: ";
+    moveCursor(13,55);
+    std::getline(std::cin, name);
+    moveCursor(17,28);
+    std::cout << WHITE_BG << BLUE << " Welcome, " << name << "! " << DFT;
+
+    moveCursor(40,0);
+    std::cout.flush();
+
+}
+
+/// @brief shows the difficulty selection page
+/// @param task the DifficultyInterface rendering task
+void ConsoleRenderer::renderDifficultyInterface (const DifficultyInterface& task){
+    std::cout << CLEAR;
+    moveCursor(3,10);
+    std::cout << " ██████╗ ███████╗███╗   ██╗███████╗██████╗  █████╗ ██╗███████╗   ██╗ ██████╗ ";
+    moveCursor(4,10);
+    std::cout << "██╔════╝ ██╔════╝████╗  ██║██╔════╝██╔══██╗██╔══██╗██║██╔════╝   ██║██╔═══██╗";
+    moveCursor(5,10);
+    std::cout << "██║  ███╗█████╗  ██╔██╗ ██║█████╗  ██████╔╝███████║██║███████╗   ██║██║   ██║";
+    moveCursor(6,10);
+    std::cout << "██║   ██║██╔══╝  ██║╚██╗██║██╔══╝  ██╔══██╗██╔══██║██║╚════██║   ██║██║   ██║";
+    moveCursor(7,10);
+    std::cout << "╚██████╔╝███████╗██║ ╚████║███████╗██║  ██║██║  ██║██║███████║██╗██║╚██████╔╝";
+    moveCursor(8,10);
+    std::cout << " ╚═════╝ ╚══════╝╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚══════╝╚═╝╚═╝ ╚═════╝ ";
+
+    moveCursor(13,28);
+    std::cout << WHITE_BG << RED << " Please choose the difficulty level: ";
+    moveCursor(17,37);
+    std::cout << WHITE_BG << BLUE << " EASY    (PRESS 1) ";
+    moveCursor(19,37);
+    std::cout << WHITE_BG << BLUE << " MEDIUM  (PRESS 2) ";
+    moveCursor(21,37);
+    std::cout << WHITE_BG << BLUE << " HARD    (PRESS 3) " << DFT;
+
+    moveCursor(40,0);
+    std::cout.flush();
+}
+
+/// @brief shows the pause page
+/// @param task the PauseInterface rendering task
+void ConsoleRenderer::renderPauseInterface (const PauseInterface& task){
+    std::cout << CLEAR;
+
+    moveCursor(3,10);
+    std::cout << " ██████╗ ███████╗███╗   ██╗███████╗██████╗  █████╗ ██╗███████╗   ██╗ ██████╗ ";
+    moveCursor(4,10);
+    std::cout << "██╔════╝ ██╔════╝████╗  ██║██╔════╝██╔══██╗██╔══██╗██║██╔════╝   ██║██╔═══██╗";
+    moveCursor(5,10);
+    std::cout << "██║  ███╗█████╗  ██╔██╗ ██║█████╗  ██████╔╝███████║██║███████╗   ██║██║   ██║";
+    moveCursor(6,10);
+    std::cout << "██║   ██║██╔══╝  ██║╚██╗██║██╔══╝  ██╔══██╗██╔══██║██║╚════██║   ██║██║   ██║";
+    moveCursor(7,10);
+    std::cout << "╚██████╔╝███████╗██║ ╚████║███████╗██║  ██║██║  ██║██║███████║██╗██║╚██████╔╝";
+    moveCursor(8,10);
+    std::cout << " ╚═════╝ ╚══════╝╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚══════╝╚═╝╚═╝ ╚═════╝ ";
+
+    moveCursor(14,30);
+    std::cout << "------------------------------";
+    moveCursor(15,30);
+    std::cout << "|                            |";
+    moveCursor(16,30);
+    std::cout << "|  Press \"P\" to resume game  |";
+    moveCursor(17,30);
+    std::cout << "|                            |";
+    moveCursor(18,30);
+    std::cout << "|   Press \"Q\" to exit game   |";
+    moveCursor(19,30);
+    std::cout << "|                            |";
+    moveCursor(20,30);
+    std::cout << "------------------------------" << DFT;
+
+    moveCursor(40,0);
+    std::cout.flush();
+}
+
+/// @brief shows the ending message when a round of game ends
+/// @param task the EndGameInterface rendering task
+void ConsoleRenderer::renderEndGameInterface (const EndGameInterface& task){
+    moveCursor(15, 30);
+    std::cout << WHITE_BG << CYAN_BG << "------------------------------";
+    moveCursor(16,30);
+    std::cout << "|                            |";
+    moveCursor(17,30);
+    if (task.isWinning){
+        std::cout << "|          You win!          |";
+    } else{
+        std::cout << "|         You lose...        |";
+    }
+    moveCursor(18,30);
+    std::cout << "| Time elapsed:" << std::setw(5) << task.timeElapsed << " seconds |";
+    moveCursor(19,30);
+    std::cout << "|     Press \"E\" to exit.     |";
+    moveCursor(20,30);
+    std::cout << "|                            |";
+    moveCursor(21,30);
+    std::cout << "------------------------------";
+
+    std::cout << DFT;
+    moveCursor(40,0);
+    std::cout.flush();
+}
+
+/// @brief shows the leaderboard
+/// @param task the LeaderboardInterface rendering task
+void ConsoleRenderer::renderLeaderboardInterface(const LeaderboardInterface& task){
+    std::cout << CLEAR;
+
+    moveCursor(3,10);
+    std::cout << " ██████╗ ███████╗███╗   ██╗███████╗██████╗  █████╗ ██╗███████╗   ██╗ ██████╗ ";
+    moveCursor(4,10);
+    std::cout << "██╔════╝ ██╔════╝████╗  ██║██╔════╝██╔══██╗██╔══██╗██║██╔════╝   ██║██╔═══██╗";
+    moveCursor(5,10);
+    std::cout << "██║  ███╗█████╗  ██╔██╗ ██║█████╗  ██████╔╝███████║██║███████╗   ██║██║   ██║";
+    moveCursor(6,10);
+    std::cout << "██║   ██║██╔══╝  ██║╚██╗██║██╔══╝  ██╔══██╗██╔══██║██║╚════██║   ██║██║   ██║";
+    moveCursor(7,10);
+    std::cout << "╚██████╔╝███████╗██║ ╚████║███████╗██║  ██║██║  ██║██║███████║██╗██║╚██████╔╝";
+    moveCursor(8,10);
+    std::cout << " ╚═════╝ ╚══════╝╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚══════╝╚═╝╚═╝ ╚═════╝ ";
+
+    moveCursor(12,6);
+    std::cout << WHITE_BG << BLUE << "EASY" << DFT;
+    moveCursor(13,6);
+    std::cout << "-----------------------------------------------------------------------------";
+    std::ifstream fin1(task.fileNameEasy);
+    std::string line,name,time;
+    for (int i = 0; i < 3; i++){
+        std::getline(fin1,line);
+        std::istringstream iss(line);
+        iss >> name;iss >> time;
+        moveCursor(14+i,6);
+        std::cout << name; 
+        moveCursor(14+i,45);
+        std::cout << time << " seconds";
+    }
+    fin1.close();
+
+    moveCursor(18,6);
+    std::cout << WHITE_BG << BLUE << "MEDIUM" << DFT;
+    moveCursor(19,6);
+    std::cout << "-----------------------------------------------------------------------------";
+    std::ifstream fin2(task.fileNameMedium);
+    for (int i = 0; i < 3; i++){
+        std::getline(fin2,line);
+        std::istringstream iss(line);
+        iss >> name;iss >> time;
+        moveCursor(20+i,6);
+        std::cout << name;
+        moveCursor(20+i,45);
+        std::cout << time << " seconds";
+    }
+    fin2.close();
+
+    moveCursor(24,6);
+    std::cout << WHITE_BG << BLUE << "HARD" << DFT;
+    moveCursor(25,6);
+    std::cout << "-----------------------------------------------------------------------------";
+    std::ifstream fin3(task.fileNameHard);
+    for (int i = 0; i < 3; i++){
+        std::getline(fin3,line);
+        std::istringstream iss(line);
+        iss >> name;iss >> time;
+        moveCursor(26+i,6);
+        std::cout << name;
+        moveCursor(26+i,45);
+        std::cout << time << " seconds";
+    }
+    fin3.close();
+
+    moveCursor(30,6);
+    std::cout << WHITE_BG << RED << "Press \"E\" to exit" << DFT;
+
+    moveCursor(40,0);
+    std::cout.flush();
+}
---
+++ RenderUpdated/main.cpp
@@ -0,0 +1,90 @@
+#include "ConsoleRenderer.h"
+#include "RenderTask.h"
+#include "Map.h" 
+ #include <iostream>
+#include <thread>
+#include <chrono>
+#include <iostream>
+#include <vector>
+#include <cctype>
+
+
+int main(){    
+    std::vector<std::unique_ptr<RenderTask>> renderQueue;
+
+
+    renderQueue.emplace_back(std::make_unique<StartInterface>());
+    renderQueue.emplace_back(std::make_unique<DifficultyInterface>());
+
+
+   
+    renderQueue.emplace_back(std::make_unique<GameInterface>());
+
+    renderQueue.emplace_back(std::make_unique<ShowMsg>("Here is the message display."));
+
+    std::vector<std::vector<Tile>> map(5, std::vector<Tile>(5));
+    for (int y = 0; y < 5; ++y) {
+        for (int x = 0; x < 5; ++x) {
+            Tile& tile = map[y][x];
+
+            tile.isMountain = (x == 1 && y == 1) || (x == 3 && y == 3);
+            tile.isCity = (x == 2 && y == 2) || (x == 0 && y == 3);
+            tile.isCapital = (x == 0 && y == 0) || (x == 4 && y == 4);
+            tile.isQuagmire = (x == 4 && y == 0) || (x == 0 && y == 4);
+
+            if (tile.isMountain) {
+                tile.owner = -1;
+                tile.army = 0;
+            } else if (x + y < 3) {
+                tile.owner = 1; 
+                tile.army = 2 + (x + y);
+            } else if (x + y > 6) {
+                tile.owner = 0; 
+                tile.army = 1 + (x * y) % 4;
+            } else {
+                tile.owner = -1;
+                tile.army = tile.isCity ? 2 : 0;
+            }
+        }
+    }
+    renderQueue.emplace_back(std::make_unique<RefreshMap>(map));
+
+    renderQueue.emplace_back(std::make_unique<PauseInterface>());
+    renderQueue.emplace_back(std::make_unique<GameInterface>());
+    renderQueue.emplace_back(std::make_unique<ShowMsg>("Here is the message display."));
+    renderQueue.emplace_back(std::make_unique<RefreshMap>(map));
+
+
+    renderQueue.emplace_back(std::make_unique<Arrow>(std::array<int,2>{0,0}, std::array<int,2>{1,0}, true, map));
+    renderQueue.emplace_back(std::make_unique<Arrow>(std::array<int,2>{0,0}, std::array<int,2>{1,0}, false, map));
+
+    Tile tile{0, 5};
+    renderQueue.emplace_back(std::make_unique<UpdateTile>(std::array<int,2>{2,0}, tile));
+
+    renderQueue.emplace_back(std::make_unique<SelectTile>(std::array<int,2>{0,1}, true, map));
+    renderQueue.emplace_back(std::make_unique<SelectTile>(std::array<int,2>{0,1}, false, map));
+
+    renderQueue.emplace_back(std::make_unique<EndGameInterface>(true, 200));
+    renderQueue.emplace_back(std::make_unique<LeaderboardInterface>("easy.txt","medium.txt","hard.txt"));
+
+    //hide cursor
+    std::cout << "\033[?25l";
+    
+    ConsoleRenderer renderer;
+
+    for (auto& task : renderQueue) {
+        renderer.render(*task);
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+
+    //at last test for the user name input
+    NameInterface ni;
+    renderer.render(ni);
+
+   
+    //show cursor
+    std::cout << "\033[?25h";
+
+
+    return 0;
+}
